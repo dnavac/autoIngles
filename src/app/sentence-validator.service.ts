ac69200227fa6129661a63b164e217468a1f20e3
DiegoN <dnavarro@araujoysegovia.com>
2023-11-21 15:46:48 -0500

    Codigo documentado

--- src/app/sentence-validator.service.ts
+++ src/app/sentence-validator.service.ts
@@ -4,26 +4,49 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SentenceValidatorService {
+
+  /**
+   * Se optienen tanto las frases validas, como invalidas del formulario
+   */
   validateSentences(sentences: string[]): { valid: string[], invalid: string[] } {
+    /**
+     * Se inicializan los arrays para almacenar las frases validas e invalidas
+     */
     const validSentences: string[] = [];
     const invalidSentences: string[] = [];
 
+    /**
+     * Expresión regular para almacenar frases válidas e invalidas
+    */
     const regex = /\b(I|You|He|She|It|We|They)\s+(am|is|are)\s+/i;
 
     for (const sentence of sentences) {
+      /**
+       * Verificar condiciones:
+       * 1. comienza con mayuscula
+       * 2.Sigue la estructura de afirmativo presente
+       * 3. Termina en punto
+       */
       const isValid = (
         sentence.charAt(0).toUpperCase() === sentence.charAt(0) &&
         regex.test(sentence) &&
         sentence.trim().charAt(sentence.length - 1) === '.'
       );
 
+      /**
+       * Se agg la frase al arrays que corresponde
+      */
       if (isValid) {
         validSentences.push(sentence);
       } else {
         invalidSentences.push(sentence);
       }
+
     }
 
+    /**
+     * Redirige al resultado con los datos
+     */
     return { valid: validSentences, invalid: invalidSentences };
   }
 }
